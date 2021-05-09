495c79ffc06839cea8be63c55e04e67024ff9c25
Freezaa9 <geoffreyharrazi@gmail.com>
2021-05-09 17:46:22 +0200

    dvc.yaml and pipeline creation

--- train.py
+++ train.py
@@ -7,33 +7,7 @@ import numpy as np
 # Set random seed
 seed = 42
 
-################################
-########## DATA PREP ###########
-################################
-
-# Load in the data
-df = pd.read_csv("velo.csv")
-
-# Prep
-
-df["datetime"] = pd.to_datetime(df["datetime"])
-df["season"] = pd.Categorical(df["season"], ordered=True)
-df["holiday"] = pd.Categorical(df["holiday"], ordered=False)
-df["workingday"] = pd.Categorical(df["workingday"], ordered=False)
-df["weather"] = pd.Categorical(df["weather"], ordered=False)
-
-df['dayofweek'] = df.datetime.dt.dayofweek
-df['hour'] = df.datetime.dt.hour
-df['month'] = df.datetime.dt.month
-df['year'] = df.datetime.dt.year
-
-df["dayofweek"] = pd.Categorical(df["dayofweek"], ordered=False)
-df["hour"] = pd.Categorical(df["hour"], ordered=False)
-df["month"] = pd.Categorical(df["month"], ordered=False)
-df["year"] = pd.Categorical(df["year"], ordered=False)
-
-df = df.drop(["casual", "registered", "datetime"], axis=1)
-
+df = pd.read_csv("velo_processed.csv")
 # Split into train and test sections
 y = df.pop("count")
 X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=seed)
